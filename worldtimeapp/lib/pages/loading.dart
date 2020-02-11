5005782b85d6aae114b5f21037120d64935a20ac
Ruwan Bandara <ruwan.se.uok@gmail.com>
2020-02-11 10:14:43 +0530

    student model create

--- worldtimeapp/lib/pages/loading.dart
+++ worldtimeapp/lib/pages/loading.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 class Loading extends StatefulWidget {
   @override
   _LoadingState createState() => _LoadingState();
@@ -11,4 +13,15 @@ class _LoadingState extends State<Loading> {
       body: Text('Loading Screen'),
     );
   }
+}
+class HttpScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+
+}
+Future<studentModel> getStudent(){
+
 }
---
+++ worldtimeapp/lib/pages/studentModel.dart
@@ -0,0 +1,9 @@
+
+class studentModel{
+  final String name;
+  final String skill;
+  final String education;
+
+  studentModel(this.name, this.skill, this.education);
+
+}
